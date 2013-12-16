2412783dc14a4588713d21403cf2bebc5598db3c
Felipe <falmeida1988@gmail.com>
2013-12-16 01:54:51 -0200

    por enquanto, vou usar só :types, em vez de :allowed_types e :disallowed_types.. se precisar depois eu adiciono outra vez...

--- models/edge.rb
+++ models/edge.rb
@@ -11,27 +11,6 @@ class Edge
     @to_node_name = to_node_name
 
 
-
-    #doesn't make sense to set both
-    if hsh.has_key?(:types_allowed) && hsh.has_key?(:types_disallowed)
-
-      # unless it's the default case obviously
-      unless hsh[:types_allowed] === :all && hsh[:types_disallowed] === :none
-        raise ArgumentError.new 'Please set either :types_allowed or :types_disallowed, but not both'
-      end
-    end
-
-    if hsh.has_key?(:types_allowed)
-      @types_allowed = hsh[:types_allowed]
-      @types_disallowed = nil
-    elsif hsh.has_key?(:types_disallowed)
-      @types_disallowed = hsh[:types_disallowed]
-      @types_allowed = nil
-    else
-      @types_allowed = :all
-      @types_disallowed = :none
-    end
-
     #setting default values
     hsh = {
         :label => 1
