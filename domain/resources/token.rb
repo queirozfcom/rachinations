414d9145d24f892151afb57404455725653e32b6
Felipe <falmeida1988@gmail.com>
2014-03-27 18:41:00 -0300

    Add locking and unlocking methods to class Token; we will use these to make sure a Token that got received this turn doesn't get passed onto another Node before next turn.

--- domain/resources/token.rb
+++ domain/resources/token.rb
@@ -1,16 +1,42 @@
 class Token
 
   def initialize
+    @lock= FALSE
   end
 
-  #hooks which can be overridden in child classes
-  def reached_node(node)
-    #do nothing
+  def lock!
+    if locked?
+      raise RuntimeError.new 'Tried to lock a locked Token.'
+    else
+      @lock = TRUE
+    end
   end
+end
 
-  def left_node(node)
-    #do nothing
+def unlock!
+  if unlocked?
+    raise RuntimeError.new 'Tried to unlock an unlocked Token.'
+  else
+    @lock = FALSE
   end
+end
+
+def locked?
+  @lock == TRUE
+end
+
+def unlocked?
+  @lock == FALSE
+end
+
+#hooks which can be overridden in child classes
+def reached_node(node)
+  #do nothing
+end
+
+def left_node(node)
+  #do nothing
+end
 
 
 end
