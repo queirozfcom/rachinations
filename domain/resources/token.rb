35d6f79275f6ae024e7a76616411f00aacc6d69b
Felipe <falmeida1988@gmail.com>
2014-03-27 18:46:29 -0300

    Correct small mistake.

--- domain/resources/token.rb
+++ domain/resources/token.rb
@@ -11,32 +11,31 @@ class Token
       @lock = TRUE
     end
   end
-end
 
-def unlock!
-  if unlocked?
-    raise RuntimeError.new 'Tried to unlock an unlocked Token.'
-  else
-    @lock = FALSE
+
+  def unlock!
+    if unlocked?
+      raise RuntimeError.new 'Tried to unlock an unlocked Token.'
+    else
+      @lock = FALSE
+    end
   end
-end
 
-def locked?
-  @lock == TRUE
-end
+  def locked?
+    @lock == TRUE
+  end
 
-def unlocked?
-  @lock == FALSE
-end
+  def unlocked?
+    @lock == FALSE
+  end
 
 #hooks which can be overridden in child classes
-def reached_node(node)
-  #do nothing
-end
+  def reached_node(node)
+    #do nothing
+  end
 
-def left_node(node)
-  #do nothing
-end
-
+  def left_node(node)
+    #do nothing
+  end
 
 end
