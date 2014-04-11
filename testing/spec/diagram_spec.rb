b1a5486839ad666d21c5e5c4aa71c9cb284409d9
Felipe <falmeida1988@gmail.com>
2014-04-11 15:05:50 -0300

    Modify tests a little bit.

--- testing/spec/diagram_spec.rb
+++ testing/spec/diagram_spec.rb
@@ -7,12 +7,13 @@ describe Diagram do
     puts "This is Diagram RSpec"
   end
 
-  it 'should allow the creation of a empty diagram' do
+  it 'can be empty' do
     d = Diagram.new 'empty'
     expect(d.name).to eq 'empty'
   end
 
-  it 'should allow the creation of a simple source-pool diagram and run n times' do
+  it 'should be created with a source and a pool and run n times' do
+
     d=Diagram.new 'simple'
 
     d.add_node! Pool, {
@@ -31,26 +32,51 @@ describe Diagram do
     }
 
     d.run!(10)
-    d.get_node('deposit').resource_count.should == 10
+
   end
 
-  it 'should allow the creation of a simple source-pool diagram and run while a condition is true' do
+
+  it 'should run with a source and a pool and have the expected amount of resources at the end' do
+
     d=Diagram.new 'simple'
+
+
+    d.add_node! Source, {
+    :name => 'source'
+    }
+
     d.add_node! Pool, {
         :name => 'deposit',
-        :initial_value => 0
     }
-    d.add_node! Source, {
-        :name => 'source'
-    }
+
     d.add_edge! Edge, {
         :name => 'connector',
         :from => 'source',
         :to => 'deposit'
     }
-    d.run_while! { d.get_node('deposit').resource_count < 10 }
+
+    d.run!(10)
     d.get_node('deposit').resource_count.should == 10
+
   end
+
+  #it 'should allow the creation of a simple source-pool diagram and run while a condition is true' do
+  #  d=Diagram.new 'simple'
+  #  d.add_node! Pool, {
+  #      :name => 'deposit',
+  #      :initial_value => 0
+  #  }
+  #  d.add_node! Source, {
+  #      :name => 'source'
+  #  }
+  #  d.add_edge! Edge, {
+  #      :name => 'connector',
+  #      :from => 'source',
+  #      :to => 'deposit'
+  #  }
+  #  d.run_while! { d.get_node('deposit').resource_count < 10 }
+  #  d.get_node('deposit').resource_count.should == 10
+  #end
 
 
 end
