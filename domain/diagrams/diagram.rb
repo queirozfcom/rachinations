9a9068d39db11465f5ed21ee5a8eb3ed810e5087
Felipe <falmeida1988@gmail.com>
2014-04-23 16:57:03 -0300

    This doesn't need to be in random order.

--- domain/diagrams/diagram.rb
+++ domain/diagrams/diagram.rb
@@ -102,7 +102,7 @@ class Diagram
     nodes.shuffle.each{ |node| node.stage! }
 
     #only after all nodes have run do we update the actual resources and changes, to be used in the next round.
-    nodes.shuffle.each{ |n| n.commit! }
+    nodes.each{ |n| n.commit! }
 
   end
 
