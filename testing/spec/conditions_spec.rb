d64fbf59971ff2288f34baa326ba40dedc249a0a
Felipe <falmeida1988@gmail.com>
2014-05-28 00:41:12 -0300

    Add spec for conditions

---
+++ testing/spec/conditions_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'spec_helper'
+
+describe 'Nodes that can be given conditions' do
+
+  it 'should do something' do
+
+    pending "will test conditions later"
+
+    true.should == false
+  end
+end
